6f9d9f2ff21451b5f74545bb4877a17bd0831950
ricardo alexandre tonietti rodrigues <rictonietti@gmail.com>
2022-11-11 09:44:54 -0300

    Atualizacao Springboot e SecurityServer

--- src/main/java/com/ead/serviceregistry/configs/WebSecurityConfig.java
+++ src/main/java/com/ead/serviceregistry/configs/WebSecurityConfig.java
@@ -3,14 +3,16 @@ package com.ead.serviceregistry.configs;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig  {
 
     @Value("${ead.serviceRegistry.username}")
     private String username;
@@ -18,8 +20,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Value("${ead.serviceRegistry.password}")
     private String password;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .httpBasic()
                 .and()
@@ -30,14 +32,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf()
                 .disable()
                 .formLogin();
+        return http.build();
     }
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth
-                .inMemoryAuthentication()
-                .withUser(username)
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails user = User.withUsername(username)
                 .password(passwordEncoder().encode(password))
-                .roles("ADMIN");
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
     @Bean
     public PasswordEncoder passwordEncoder(){
